f42d1ac5218c1ef6d137e21f401978f700dbd4d4
not-elm <elmgameinfo@gmail.com>
2025-06-01 19:13:11 +0900

    fmt

--- examples/vrma.rs
+++ examples/vrma.rs
@@ -34,8 +34,7 @@ fn spawn_vrm(
     commands
         .spawn(VrmHandle(asset_server.load("vrm/AliciaSolid.vrm")))
         .with_children(|cmd| {
-            cmd
-                .spawn(VrmaHandle(asset_server.load("vrma/VRMA_01.vrma")))
+            cmd.spawn(VrmaHandle(asset_server.load("vrma/VRMA_01.vrma")))
                 .observe(apply_play_vrma);
         });
 }
